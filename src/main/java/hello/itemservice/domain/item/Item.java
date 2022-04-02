fee124a76b30601a8a6a969550c01cfa0621b139
This2sho <zx00018@naver.com>
2022-04-02 23:51:05 +0900

    Chore: domain 객체 item 이름 변경

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -3,17 +3,17 @@ package hello.itemservice.domain.item;
 import lombok.Data;
 
 @Data
-public class item {
+public class Item {
 
     private Long id;
     private String itemName;
     private Integer price;
     private Integer quantity;
 
-    public item() {
+    public Item() {
     }
 
-    public item(Long id, String itemName, Integer price, Integer quantity) {
+    public Item(Long id, String itemName, Integer price, Integer quantity) {
         this.id = id;
         this.itemName = itemName;
         this.price = price;
